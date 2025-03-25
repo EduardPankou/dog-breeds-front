0238c629e603c1bfba3385cf2cd9145e082447a0
Pankov <eduardpanov660@gmail.com>
2025-03-25 11:10:40 +0300

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>header</header>
+    <slot/>
+    <footer>footer</footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
